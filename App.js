90c6b01a59f52bfab89375441b80abf178bf81ad
“名hie <s.hironori.1212@icloud.com>
2020-04-05 10:32:20 +0900

    Create MemoList

--- App.js
+++ App.js
@@ -1,116 +1,126 @@
 import React from 'react';
-import {
-    View,
-    Text
-} from 'react-native';
+import {StyleSheet, Text, View } from 'react-native';
 
-export default class App extends React.Component{
-    render(){
-        return (
-            <View style={
-                {
-                    "alignItems": "flex-start",
-                    "paddingStart": 46,
-                    "paddingTop": 113,
-                    "flex": 1
-                }
-            } >
-                <Text style={
-                    {
-                        
-                        "fontSize": 20,
-                        "color": "rgba(0, 0, 0, 255)",
-                        "marginStart": 104
-                    }
-                } > LOGIN </Text>
-                <View style={
-                    {
-                        "alignItems": "flex-start",
-                        "marginTop": 73
-                    }
-                } >
-                    <View style={
-                        {
-                            "alignItems": "flex-start"
-                        }
-                    } >
-                        <View style={
-                            {
-                                "alignItems": "flex-start",
-                                "paddingStart": 14,
-                                "paddingTop": 10,
-                                "width": 284,
-                                "height": 46,
-                                "borderRadius": 20,
-                                "borderWidth": 1,
-                                "borderColor": "rgba(112, 112, 112, 255)",
-                                "backgroundColor": "rgba(249, 249, 249, 255)"
-                            }
-                        } >
-                            <Text style={
-                                {
-                                    
-                                    "fontSize": 30,
-                                    "color": "rgba(112, 112, 112, 255)"
-                                }
-                            } > Email </Text>
-                        </View>
-                    </View>
-                    <View style={
-                        {
-                            "alignItems": "flex-start",
-                            "marginTop": 46
-                        }
-                    } >
-                        <View style={
-                            {
-                                "alignItems": "flex-start",
-                                "paddingStart": 14,
-                                "paddingTop": 10,
-                                "width": 284,
-                                "height": 46,
-                                "borderRadius": 20,
-                                "borderWidth": 1,
-                                "borderColor": "rgba(112, 112, 112, 255)",
-                                "backgroundColor": "rgba(249, 249, 249, 255)"
-                            }
-                        } >
-                            <Text style={
-                                {
-                                    
-                                    "fontSize": 20,
-                                    "color": "rgba(112, 112, 112, 255)"
-                                }
-                            } > Password </Text>
-                        </View>
+import BodyText from './src/elements/BodyText';
+
+export default class App extends React.Component {
+    render() {
+        return(
+            <View style={styles.container}>
+                <View style={styles.appbar}>
+                    <View>
+                        <Text style={styles.appbarText}>Memo</Text>
                     </View>
                 </View>
-                <View style={
-                    {
-                        "alignItems": "flex-start",
-                        "paddingStart": 51,
-                        "paddingTop": 5,
-                        "marginStart": 62,
-                        "marginTop": 51,
-                        "width": 160,
-                        "height": 42,
-                        "borderRadius": 20,
-                        "borderWidth": 1,
-                        "borderColor": "rgba(112, 112, 112, 255)",
-                        "backgroundColor": "rgba(255, 255, 255, 255)"
-                    }
-                } >
-                    <Text style={
-                        {
-                            
-                            "fontSize": 24,
-                            "color": "rgba(0, 0, 0, 255)"
-                        }
-                    } > Login </Text>
+
+                <View style={styles.memoList}>
+                    <View style={styles.memoListItem}>
+                        <Text style={styles.memoTitle}>講座のアイテム</Text>
+                        <Text style={styles.memoDate}>2020/04/04</Text>
+                    </View>
+
+                    <View style={styles.memoListItem}>
+                        <Text style={styles.memoTitle}>講座のアイテム</Text>
+                        <Text style={styles.memoDate}>2020/04/04</Text>
+                    </View>
+
+                    <View style={styles.memoListItem}>
+                        <Text style={styles.memoTitle}>講座のアイテム</Text>
+                        <Text style={styles.memoDate}>2020/04/04</Text>
+                    </View>
+
+                    <View style={styles.memoListItem}>
+                        <Text style={styles.memoTitle}>講座のアイテム</Text>
+                        <Text style={styles.memoDate}>2020/04/04</Text>
+                    </View>
+
+                    <View style={styles.memoListItem}>
+                        <Text style={styles.memoTitle}>講座のアイテム</Text>
+                        <Text style={styles.memoDate}>2020/04/04</Text>
+                    </View>
+
+                    <View style={styles.memoAddButton}>
+                        <Text style={styles.memoAddButtonTitle}>+</Text>
+                    </View>
                 </View>
             </View>
-    
         );
-    };
+    }
 }
 
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#D2D6D9',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+
+    appbar: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        height: 78,
+        paddingTop: 30,
+        backgroundColor: '#D96666',
+        justifyContent: 'center',
+        alignItems: 'center',
+        zIndex: 10,
+        shadowColor: '#000',
+        shadowOffset: {width: 0, height: 0},
+        shadowOpacity: 0.5,
+        shadowRadius: 3,
+    },
+
+    appbarText: {
+        color: '#fff',
+        fontSize: 18,
+    },
+
+    memoList: {
+        width: '100%',
+        backgroundColor: '#DDD',
+        flex: 1,
+        paddingTop: 78,
+    },
+
+    memoListItem: {
+        padding: 16,
+        borderBottomWidth: 1,
+        borderBottomColor: '#ddd',
+        backgroundColor: '#fff'
+    },
+
+    memoTitle: {
+        fontSize: 18,
+        marginBottom: 4,
+    },
+
+    memoDate: {
+        fontSize: 12,
+        color: '#707070',
+    },
+
+    memoAddButton: {
+        position: 'absolute',
+        bottom: 32,
+        right: 32,
+        width: 48,
+        height: 48,
+        backgroundColor: '#463973',
+        borderRadius: 24,
+        justifyContent: 'center',
+        alignItems: 'center',
+        shadowColor: '#000',
+        shadowOffset: {width: 0, height: 2},
+        shadowOpacity: 0.3,
+        shadowRadius: 3,
+    },
+
+    memoAddButtonTitle: {
+        fontSize: 30,
+        color: '#FFFFFF',
+        lineHeight: 30,
+    }
+});
